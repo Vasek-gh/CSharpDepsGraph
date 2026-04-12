a19171303ba0c00f85f8d720a3c2d57f229254a3
Vasek <my.super.g.email@gmail.com>
2026-04-12 23:23:11 +0300

    -

--- tests/CSharpDepsGraph.Tests/AsmName.cs
+++ tests/CSharpDepsGraph.Tests/AsmName.cs
@@ -2,14 +2,13 @@ namespace CSharpDepsGraph.Tests;
 
 internal class AsmName
 {
-    private const string _actualNetVersion = "10.0.0.0";
+    private const string _actualNetVersion = "8.0.0.0";
 
     public const string Test = "Test";
     public const string TestProject = "TestProject";
     public const string TestProjectCli = "TestProject.Cli";
 
     public const string CoreLib = $"System.Private.CoreLib_{_actualNetVersion}";
-    public const string CoreLib80 = "System.Private.CoreLib_8.0.0.0";
 
     public const string Runtime = "System.Runtime";
     public const string Runtime60 = "System.Runtime_6.0.0.0";
