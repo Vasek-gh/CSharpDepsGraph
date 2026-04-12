2540e4cfa920497398545c736244ae7a0dd91dc6
Vasek <my.super.g.email@gmail.com>
2026-04-12 15:03:43 +0300

    -

--- tests/CSharpDepsGraph.Tests/Integrations/ProjectSource.cs
+++ tests/CSharpDepsGraph.Tests/Integrations/ProjectSource.cs
@@ -1,7 +1,6 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.MSBuild;
 using NUnit.Framework;
-using System.Diagnostics;
 
 namespace CSharpDepsGraph.Tests.Integrations;
 
@@ -14,8 +13,6 @@ public static class ProjectSource
     [OneTimeSetUp]
     public static async Task Init()
     {
-        //await BuildSolution(TestData.TestProjectSolution);
-
         Workspace = MSBuildWorkspace.Create();
         Solution = await Workspace.OpenSolutionAsync(TestData.TestProjectSolution);
     }
@@ -24,47 +21,5 @@ public static class ProjectSource
     public static void Done()
     {
         Workspace.Dispose();
-    }
-
-    private static async Task BuildSolution(string slnFileName)
-    {
-        if (Environment.GetEnvironmentVariable(TestData.SkipBuildVar) == "1")
-        {
-            await TestContext.Out.WriteLineAsync("Build test data skipped");
-            return;
-        }
-
-        await TestContext.Out.WriteLineAsync($"Start build test data {slnFileName}...");
-
-        using var process = new Process()
-        {
-            StartInfo = new ProcessStartInfo()
-            {
-                FileName = "dotnet",
-                Arguments = $"build {slnFileName} --nologo -v n --disable-build-servers",
-                RedirectStandardError = true,
-                RedirectStandardOutput = true,
-                UseShellExecute = false,
-                CreateNoWindow = true,
-                Environment = {
-                    { "DOTNET_NOLOGO", "1" },
-                    { "DOTNET_CLI_UI_LANGUAGE", "en" },
-                    { "DOTNET_CLI_TELEMETRY_OPTOUT", "1" },
-                    { "DOTNET_SKIP_FIRST_TIME_EXPERIENCE", "1" }
-                }
-            }
-        };
-
-        if (!process.Start())
-        {
-            throw new Exception("Fail to start build process");
-        }
-
-        await process.WaitForExitAsync();
-        if (process.ExitCode != 0)
-        {
-            var output = await process.StandardOutput.ReadToEndAsync();
-            throw new Exception($"Build test data fail: {output}");
-        }
     }
 }
--- tests/CSharpDepsGraph.Tests/TestData.cs
+++ tests/CSharpDepsGraph.Tests/TestData.cs
@@ -7,6 +7,4 @@ public static class TestData
     public static readonly string Path = System.IO.Path.GetFullPath("TestData");
     public static readonly string TestProjectPath = System.IO.Path.Combine(Path, TestProjectName);
     public static readonly string TestProjectSolution = System.IO.Path.Combine(Path, TestProjectName + ".sln");
-
-    public const string SkipBuildVar = "SKIP_BUILD";
 }
